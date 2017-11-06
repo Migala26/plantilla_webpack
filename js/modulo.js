be389591caf9746526ee01d94b08f8ddd400c29f
Otto Colomina <otto@dccia.ua.es>
2017-11-06 22:43:33 +0100

    comentarios sobre export

--- js/modulo.js
+++ js/modulo.js
@@ -2,4 +2,6 @@ function saludar(nombre) {
     return "Hola qué tal, " + nombre
 }
 
+//Esto es un export con nombre, donde queramos importarlo hay que hacerlo con el mismo nombre: import {saludar}
+//Para hacerlo sin nombre -> export default saludar, luego se puede importar con import nombre_arbitrario
 export {saludar}
