e8a24505be0aa69c97b4d81f86aef57c2f765bdd
Otto Colomina Pardo <otto@dccia.ua.es>
2017-11-09 10:08:18 +0100

    reestructurado js para que se vea mejor cómo interactúan main.js y los módulos importados

--- js/main.js
+++ js/main.js
@@ -1,17 +1,13 @@
+//Este es el "archivo inicial" tal y como está configurado en webpack
+//La ejecución de js comenzaría por aquí.
+//importamos los componentes y los pintamos en el HTML con ReactDOM.render
+
+//Al ser un export default, al importarlo podemos cambiarle el nombre
 import React from 'react'
 import ReactDOM from 'react-dom'  
+import Saludo from './ComponenteSaludo'
 
-import {saludar} from './modulo'
-
-class ComponenteSaludo extends React.Component {
-    render() {
-        return <div>
-            {saludar(this.props.nombre)}
-        </div> 
-    }
-}
-
-ReactDOM.render(<ComponenteSaludo nombre="Pepe"/>
+ReactDOM.render(<Saludo nombre="Pepe"/>
             , document.getElementById('componente'));  
 
 
--- js/modulo.js
+++ js/modulo.js
@@ -2,6 +2,6 @@ function saludar(nombre) {
     return "Hola qué tal, " + nombre
 }
 
-//Esto es un export con nombre, donde queramos importarlo hay que hacerlo con el mismo nombre: import {saludar}
-//Para hacerlo sin nombre -> export default saludar, luego se puede importar con import nombre_arbitrario
+//Esto es un export con nombre, donde queramos importarlo hay que hacerlo
+//con el mismo nombre: import {saludar}
 export {saludar}
